154cb4874d54008a1db54478883b38158e7edb87
Zerkyn <Zerkyn.55@gmail.com>
2024-06-17 19:43:40 -0600

    played with command responses in ping.js

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,10 +1,22 @@
 const {SlashCommandBuilder} = require('discord.js')
+const wait = require('node:timers/promises').setTimeout
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        await interaction.reply('Pong!')
+        await interaction.reply({ content: 'Pong!', ephemeral: true})
+        
+        //to edit a reply
+        // await wait(2_000)
+        // await interaction.editReply({ content: 'Pong again!', ephemeral: true})
+        
+        // to defer reply if longer than 3 seconds is needed
+        // await interaction.deferReply()
+        // await wait(4_000)
+        // await interaction.editReply('Pong!')
+
+        // await interaction.followUp('Pong again!')
     },
 }
